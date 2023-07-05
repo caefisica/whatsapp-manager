889c036ffcd43c2623e787b12a9ba64670d7470d
David Duran <dadch1404@gmail.com>
2023-07-05 13:08:37 -0500

    feat: :sparkles: retry behaviour & refactoring

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const cache = new NodeCache();
 const { dropAuth } = require('./utils/reload');
 const { insertMessage } = require('./db');
 const commands = require('./commands');
+const retry = require('async-retry');
 
 // Constants
 const {
@@ -33,6 +34,8 @@ let startCount = 1;
 
 // Restart behavior
 let retryCount = 0;
+// Retry behavior
+const MESSAGE_PROCESSING_MAX_TRIES = 5;
 
 // Accepted message types
 const acceptedTypes = new Set([
@@ -83,118 +86,163 @@ function handleMessageUpsert(sock) {
     return async (m) => {
         const msg = JSON.parse(JSON.stringify(m)).messages[0];
 
-        const from = msg.key.remoteJid;
-        const isGroup = from.endsWith('@g.us');
-        let senderNumber = isGroup ? (msg.key.participant || from) : from;
+        const messageObject = await getMessageObject(sock, msg);
 
-        if (senderNumber.includes(':')) { // remove : from number
-            senderNumber = senderNumber.slice(0, senderNumber.search(':')) + senderNumber.slice(senderNumber.search('@'));
-        }
-
-        let groupMetadata = '';
-        if (isGroup) {
-            groupMetadata = cache.get(from + ':groupMetadata');
-            if (!groupMetadata) {
-                groupMetadata = await sock.groupMetadata(from);
-                cache.set(from + ':groupMetadata', groupMetadata, 60 * 60); // Cache group metadata for 1 hour
-            }
-        } else {
+        if (!shouldProcessMessage(messageObject)) {
             return;
         }
-
-        if (!msg.message) {
-            return;
-        }
-
-        const isFromMe = msg.key.fromMe;
-        const senderName = msg.pushName;
-        const groupName = isGroup ? groupMetadata.subject : '';
-        const isPremiumUser = premiumUsers.includes(senderNumber);
-        const isAdmin = adminUsers.includes(senderNumber);
-
-        let commandPrefix;
-        if (isAdmin) {
-            commandPrefix = ownerCommandPrefix; 
-        } else if (isPremiumUser) {
-            commandPrefix = premiumCommandPrefix;
-        } else {
-            commandPrefix = generalCommandPrefix;
-        }
-
-        const type = Object.keys(msg.message)[0];
-        if (!acceptedTypes.has(type)) {
-            console.log('Message type not accepted');
-            console.log('type:', type);
-            return;
-        }
-
-        if ((type === 'imageMessage' && !msg.message[type]?.caption) || (type === 'videoMessage' && !msg.message[type]?.caption)) {
-            console.log('No caption found on image/video message, ignoring');
-            return;
-        }
-
-        let textMessage = msg.message[type]?.caption || msg.message[type]?.text || msg.message[type] || '';
-        console.log('textMessage:', textMessage);
-        console.log('type:', type)
-        textMessage = textMessage.replace(/\n|\r/g, ''); //remove all \n and \r
-
-        if (!textMessage.startsWith(commandPrefix)) {
-            return;
-        }
-
-        const messageObject = {
-            isDocument: type === 'documentWithCaptionMessage',
-            isVideo: type === 'videoMessage',
-            isImage: type === 'imageMessage',
-            isSticker: type === 'stickerMessage',
-            messageType: type,
-            groupName,
-            from,
-            senderName,
-            senderNumber,
-            isFromMe,
-            isPremiumUser,
-            isAdmin,
-            commandPrefix,
-            textMessage,
-            botEmoji,
-        };
-
-        const [name, ...args] = textMessage.split(' ');
 
         try {
-            const commandSet = isAdmin ? commands.owner : isPremiumUser ? commands.premium : commands.general;
-            const commandName = name.substring(1).toLowerCase();
-
-            if (commandSet[commandName]) {
-                const promises = [];
-                promises.push(commandSet[commandName].handler(sock, msg, messageObject, args));
-
-                // React to the message after the command has been processed
-                const reactionMessage = {
-                    react: {
-                        text: completeEmoji,
-                        key: msg.key
-                    }
-                };
-                promises.push(sock.sendMessage(senderNumber, reactionMessage));
-
-                await Promise.all(promises);
-            } else {
-                throw new Error(`Unrecognized textMessage: ${commandName}`);
-            }
+            await retry(async () => {
+                await processCommand(sock, msg, messageObject);
+            }, {
+                retries: MESSAGE_PROCESSING_MAX_TRIES,
+                onRetry: (error) => {
+                    console.error('Error processing message, retrying:', error);
+                },
+            });
         } catch (error) {
             console.error('Error processing message:', error);
-
-            // Send error message to database
-            const timestamp = new Date();
-            await insertMessage(
-                JSON.stringify(messageObject),
-                senderNumber,
-                timestamp
-            ).catch(console.error);
+            await logMessageToDatabase(messageObject, error);
         }
     };
+}
+
+async function getMessageObject(sock, msg) {
+    const from = msg.key.remoteJid;
+    const isGroup = from.endsWith('@g.us');
+    let senderNumber = isGroup ? (msg.key.participant || from) : from; // sender number can be individual or group
+
+    if (senderNumber.includes(':')) { // some users have a ":", we remove it
+        senderNumber = senderNumber.slice(0, senderNumber.search(':')) + senderNumber.slice(senderNumber.search('@'));
+    }
+
+    // Extract and cache group metadata
+    let groupMetadata = '';
+    if (isGroup) {
+        groupMetadata = cache.get(from + ':groupMetadata');
+        if (!groupMetadata) {
+            groupMetadata = await sock.groupMetadata(from);
+            cache.set(from + ':groupMetadata', groupMetadata, 60 * 60); // Cache group metadata for 1 hour
+        }
+    } else {
+        // Ignore messages from non-group chats
+        return;
+    }
+
+    if (!msg.message) {
+        // Some unencrypted messages don't have a message object, ignore them
+        // Maybe this is fixed after removing makeInMemoryStore
+        return;
+    }
+
+    const isFromMe = msg.key.fromMe;
+    const senderName = msg.pushName;
+    const groupName = isGroup ? groupMetadata.subject : '';
+    const isPremiumUser = premiumUsers.includes(senderNumber);
+    const isAdmin = adminUsers.includes(senderNumber);
+
+    let commandPrefix;
+    if (isAdmin) {
+        commandPrefix = ownerCommandPrefix; 
+    } else if (isPremiumUser) {
+        commandPrefix = premiumCommandPrefix;
+    } else {
+        commandPrefix = generalCommandPrefix;
+    }
+
+    const type = Object.keys(msg.message)[0];
+    if (!acceptedTypes.has(type)) {
+        console.log('Message type not accepted', type);
+        return;
+    }
+
+    if ((type === 'imageMessage' && !msg.message[type]?.caption) || (type === 'videoMessage' && !msg.message[type]?.caption)) {
+        console.log('Ignoring messages/videos without caption');
+        return;
+    }
+
+    let textMessage = msg.message[type]?.caption || msg.message[type]?.text || msg.message[type] || '';
+    textMessage = textMessage.replace(/\n|\r/g, ''); //remove all \n and \r
+
+    return {
+        isDocument: type === 'documentWithCaptionMessage',
+        isVideo: type === 'videoMessage',
+        isImage: type === 'imageMessage',
+        isSticker: type === 'stickerMessage',
+        messageType: type,
+        groupName,
+        from,
+        senderName,
+        senderNumber,
+        isFromMe,
+        isPremiumUser,
+        isAdmin,
+        commandPrefix,
+        textMessage,
+        botEmoji,
+    };
+}
+
+function shouldProcessMessage(messageObject) {
+    if (!messageObject) {
+        // Ignore if messageObject is undefined (stickers and videos are causing some issues with textMessage)
+        return false;
+    }
+
+    if (!messageObject.textMessage.startsWith(messageObject.commandPrefix)) {
+        return false;
+    }
+
+    return true;
+}
+
+async function processCommand(sock, msg, messageObject) {
+    const [name, ...args] = messageObject.textMessage.split(' ');
+    const commandSet = await getCommandSet(messageObject);
+    const commandName = name.substring(1).toLowerCase();
+
+    if (!commandSet[commandName]) {
+        throw new Error(`Unrecognized textMessage: ${commandName}`);
+    }
+
+    await Promise.all([
+        commandSet[commandName].handler(sock, msg, messageObject, args),
+        sendReaction(sock, msg, messageObject),
+    ]);
+}
+
+function getCommandSet(messageObject) {
+    if (messageObject.isAdmin) {
+        return commands.owner;
+    }
+
+    if (messageObject.isPremiumUser) {
+        return commands.premium;
+    }
+
+    return commands.general;
+}
+
+async function sendReaction(sock, msg, messageObject) {
+    const reactionMessage = {
+        react: {
+            text: completeEmoji,
+            key: msg.key,
+        },
+    };
+
+    await sock.sendMessage(messageObject.senderNumber, reactionMessage);
+}
+
+async function logMessageToDatabase(messageObject, error) {
+    const timestamp = new Date();
+    await insertMessage(
+        JSON.stringify(messageObject),
+        messageObject.senderNumber,
+        timestamp,
+        error,
+    ).catch(console.error);
 }
 
 function handleConnectionUpdate(sock) {
