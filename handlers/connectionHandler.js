02ee5ef3ebe4e365e9869e2480bf2e51037f4434
David Duran <dadch1404@gmail.com>
2023-07-05 13:43:50 -0500

    refactor: :art: modularization of the code

---
+++ handlers/connectionHandler.js
@@ -0,0 +1,63 @@
+const {
+    DisconnectReason,
+} = require('@adiwajshing/baileys');
+const { start } = require('../index');
+const { 
+    myNumberWithJid,
+    MAX_RETRIES,
+} = require('../config/constants');
+
+const { dropAuth } = require('../utils/reload');
+
+// Statistics
+let startCount = 1;
+
+// Restart behavior
+let retryCount = 0;
+
+function handleConnectionUpdate(sock) {
+    return async (update) => {
+        const { connection, lastDisconnect } = update;
+        const isStatusCodeLogout = lastDisconnect?.error?.output?.statusCode === DisconnectReason.loggedOut;
+        const shouldReconnect = lastDisconnect?.error && !isStatusCodeLogout;
+
+        try {
+            switch (connection) {
+            case 'open':
+                console.log('[LOG] El bot está listo para usar');
+                retryCount = 0; // Reset retry count on successful connection
+                await sock.sendMessage(myNumberWithJid, {
+                    text: `[INICIO] - ${startCount}`,
+                });
+                break;
+            case 'close':
+                if (shouldReconnect) {
+                    if (retryCount < MAX_RETRIES) {
+                        const delay = Math.pow(2, retryCount) * 1000; // Exponential backoff delay
+                        console.log(`[ALERTA] La conexión fue CERRADA ${lastDisconnect.error}, reintentando en ${delay/1000} segundos...`);
+                        retryCount++;
+                        startCount++;
+                        setTimeout(start, delay);
+                    } else {
+                        console.error('[PROBLEMA] Se ha superado el número máximo de reintentos. Reinicie el bot manualmente');
+                    }
+                } else {
+                    console.log('[PROBLEMA] Estás desconectado. Prepárate para escanear el código QR');
+                    await dropAuth();
+                    startCount++; // increment startCount as bot is attempting to reconnect
+                    setTimeout(start, 1000 * 5);
+                }
+                break;
+            default:
+                console.log('[LOG] Este evento de actualización es desconocido:', update);
+                break;
+            }
+        } catch (error) {
+            console.error('Error en connection.update', error, update);
+        }
+    };
+}
+
+module.exports = {
+    handleConnectionUpdate,
+};
---
+++ handlers/messageHandler.js
@@ -0,0 +1,199 @@
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
+const { insertMessage } = require('../db');
+const commands = require('../commands');
+const retry = require('async-retry');
+
+// Constants
+const {
+    generalCommandPrefix,
+    premiumCommandPrefix,
+    ownerCommandPrefix,
+    botEmoji,
+    completeEmoji,
+    premiumUsers,
+    adminUsers,
+} = require('../config/constants');
+
+// Retry behavior
+const MESSAGE_PROCESSING_MAX_TRIES = 5;
+
+// Accepted message types
+const acceptedTypes = new Set([
+    'conversation',
+    'textMessage',
+    'imageMessage',
+    'videoMessage',
+    // 'stickerMessage',
+    'documentWithCaptionMessage',
+    'extendedTextMessage',
+]);
+
+function handleMessageUpsert(sock) {
+    return async (m) => {
+        const msg = JSON.parse(JSON.stringify(m)).messages[0];
+
+        const messageObject = await getMessageObject(sock, msg);
+
+        if (!shouldProcessMessage(messageObject)) {
+            return;
+        }
+
+        try {
+            await retry(async () => {
+                await processCommand(sock, msg, messageObject);
+            }, {
+                retries: MESSAGE_PROCESSING_MAX_TRIES,
+                onRetry: (error) => {
+                    console.error('Error processing message, retrying:', error);
+                },
+            });
+        } catch (error) {
+            console.error('Error processing message:', error);
+            await logMessageToDatabase(messageObject, error);
+        }
+    };
+}
+
+async function getMessageObject(sock, msg) {
+    const from = msg.key.remoteJid;
+    const isGroup = from.endsWith('@g.us');
+    let senderNumber = isGroup ? (msg.key.participant || from) : from; // sender number can be individual or group
+
+    if (senderNumber.includes(':')) { // some users have a ":", we remove it
+        senderNumber = senderNumber.slice(0, senderNumber.search(':')) + senderNumber.slice(senderNumber.search('@'));
+    }
+
+    // Extract and cache group metadata
+    let groupMetadata = '';
+    if (isGroup) {
+        groupMetadata = cache.get(from + ':groupMetadata');
+        if (!groupMetadata) {
+            groupMetadata = await sock.groupMetadata(from);
+            cache.set(from + ':groupMetadata', groupMetadata, 60 * 60); // Cache group metadata for 1 hour
+        }
+    } else {
+        // Ignore messages from non-group chats
+        return;
+    }
+
+    if (!msg.message) {
+        // Some unencrypted messages don't have a message object, ignore them
+        // Maybe this is fixed after removing makeInMemoryStore
+        return;
+    }
+
+    const isFromMe = msg.key.fromMe;
+    const senderName = msg.pushName;
+    const groupName = isGroup ? groupMetadata.subject : '';
+    const isPremiumUser = premiumUsers.includes(senderNumber);
+    const isAdmin = adminUsers.includes(senderNumber);
+
+    let commandPrefix;
+    if (isAdmin) {
+        commandPrefix = ownerCommandPrefix; 
+    } else if (isPremiumUser) {
+        commandPrefix = premiumCommandPrefix;
+    } else {
+        commandPrefix = generalCommandPrefix;
+    }
+
+    const type = Object.keys(msg.message)[0];
+    if (!acceptedTypes.has(type)) {
+        console.log('Message type not accepted', type);
+        return;
+    }
+
+    if ((type === 'imageMessage' && !msg.message[type]?.caption) || (type === 'videoMessage' && !msg.message[type]?.caption)) {
+        console.log('Ignoring messages/videos without caption');
+        return;
+    }
+
+    let textMessage = msg.message[type]?.caption || msg.message[type]?.text || msg.message[type] || '';
+    textMessage = textMessage.replace(/\n|\r/g, ''); //remove all \n and \r
+
+    return {
+        isDocument: type === 'documentWithCaptionMessage',
+        isVideo: type === 'videoMessage',
+        isImage: type === 'imageMessage',
+        isSticker: type === 'stickerMessage',
+        messageType: type,
+        groupName,
+        from,
+        senderName,
+        senderNumber,
+        isFromMe,
+        isPremiumUser,
+        isAdmin,
+        commandPrefix,
+        textMessage,
+        botEmoji,
+    };
+}
+
+function shouldProcessMessage(messageObject) {
+    if (!messageObject) {
+        // Ignore if messageObject is undefined (stickers and videos are causing some issues with textMessage)
+        return false;
+    }
+
+    if (!messageObject.textMessage.startsWith(messageObject.commandPrefix)) {
+        return false;
+    }
+
+    return true;
+}
+
+async function processCommand(sock, msg, messageObject) {
+    const [name, ...args] = messageObject.textMessage.split(' ');
+    const commandSet = await getCommandSet(messageObject);
+    const commandName = name.substring(1).toLowerCase();
+
+    if (!commandSet[commandName]) {
+        console.error(`Unrecognized textMessage: ${commandName}`);
+        await logMessageToDatabase(messageObject, new Error(`Unrecognized textMessage: ${commandName}`));
+        return;
+    }
+
+    await Promise.all([
+        commandSet[commandName].handler(sock, msg, messageObject, args),
+        sendReaction(sock, msg, messageObject),
+    ]);
+}
+
+function getCommandSet(messageObject) {
+    if (messageObject.isAdmin) {
+        return {...commands.general, ...commands.premium, ...commands.owner};
+    }
+
+    if (messageObject.isPremiumUser) {
+        return {...commands.general, ...commands.premium};
+    }
+
+    return commands.general;
+}
+
+async function sendReaction(sock, msg, messageObject) {
+    const reactionMessage = {
+        react: {
+            text: completeEmoji,
+            key: msg.key,
+        },
+    };
+
+    await sock.sendMessage(messageObject.senderNumber, reactionMessage);
+}
+
+async function logMessageToDatabase(messageObject, error) {
+    const timestamp = new Date();
+    await insertMessage(
+        JSON.stringify(messageObject),
+        messageObject.senderNumber,
+        timestamp,
+        error,
+    ).catch(console.error);
+}
+
+module.exports = {
+    handleMessageUpsert,
+};
