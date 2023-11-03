bdfc9a64028639c6d15a50f830d37a5f63c340f2
David Duran <dadch1404@gmail.com>
2023-11-03 06:46:21 -0500

    Remove unused code

--- src/commands/general/translate.js
+++
@@ -1,8 +0,0 @@
-const { translate } = require('@vitalets/google-translate-api');
-
-async function translateText(textToBeTranslated, targetLang = 'en') {
-    const { text } = await translate(textToBeTranslated, { from: 'es', to: targetLang });
-    return text;
-}
-
-module.exports = { translateText };
